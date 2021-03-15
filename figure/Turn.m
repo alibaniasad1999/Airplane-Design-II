92b78686242739b5279e003fff4d3dca99d117ee
unknown <alibaniasad1999@yahoo.com>
2021-03-15 16:12:03 +0330

    add old exels and add turn matlab file

---
+++ figure/Turn.m
@@ -0,0 +1,18 @@
+%%% Load Data
+% Load 50 seat Turn phase Data from exel
+TurnData50 = readtable('Turn Maneuver.xlsx', 'sheet', '50 seat');
+% Load 76 seat Turn phase Data from exel
+TurnData76 = readtable('Turn Maneuver.xlsx', 'sheet', '76 seat');
+%%%%%%% Using Data %%%%%%%
+% For All T/W Phase of Aircraft - Technology Level - Seat Num
+% For all W/S phase seat
+%%%%%%%%% 50 Seat %%%%%%%%%
+Turn_50 = TurnData50.W_S;
+Turn_low_50 = TurnData50.T_W;
+Turn_mid_50 = TurnData50.T_W_1;
+Turn_high_50 = TurnData50.T_W_2;
+%%%%%%%%% 50 Seat %%%%%%%%%
+Turn_76 = TurnData76.W_S;
+Turn_low_76 = TurnData76.T_W;
+Turn_mid_76 = TurnData76.T_W_1;
+Turn_high_76 = TurnData76.T_W_2;
