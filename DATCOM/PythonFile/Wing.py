55d0c81f5192b0e3e77797cb9f4db23790326fd7
Ali <alibaniasad1999@yahoo.com>
2021-03-28 02:08:38 +0430

    add wing

---
+++ DATCOM/PythonFile/Wing.py
@@ -0,0 +1,42 @@
+# add wing files to DATCOM #
+# $WGPLNF
+# CHRDTP = 1.367, ,
+# SSPNE = 11.34, SSPN = 12.75,
+# SAVSI = 1.18, CHSTAT = 0.25,
+# TWISTA = -1.2, DHDADI = 1.0,
+# TYPE = 1.0$
+# Tip Chord
+CHRDTP = 1.367
+# Root Chord
+CHRDR = 2.96
+# Semi Span (Exposed)
+SSPNE = 11.34
+# Semi Span (Theoretical)
+SSPN = 12.75
+# Sweep Angle
+SAVSI = 35
+# Refrence chord station for inbord and outboard panel sweep angles, fraction of chord
+CHSTAT = 0.25
+# Twist angle (negative L.E rotated down)
+TWISTA = -1.2
+# Dihedral Angle
+DHDADI = 1.0
+# TYPE = 1.0 straight tapered platform
+# TYPE = 2.0 double delta platform AR<3
+# TYPE = 3.0 Cranked platform AR>3
+TYPE = 1.0
+WingAirfoil = 'NACA-W-6-65-210'
+def wing_writer(file):
+    # Name list
+    file.write('$WGPLNF ')
+    file.write('CHRDTP = %s, ' % str(CHRDTP))
+    file.write('CHRDR = %s,\n\t\t' % str(CHRDR))
+    file.write('SSPNE = %s, ' % str(SSPNE))
+    file.write('SSPN = %s,\n\t\t' % str(SSPN))
+    file.write('SAVSI = %s, ' % str(SAVSI))
+    file.write('CHSTAT = %s,\n\t\t' % str(CHSTAT))
+    file.write('TWISTA = %s, ' % str(TWISTA))
+    file.write('DHDADI = %s,\n\t\t' % str(DHDADI))
+    file.write('TYPE = %s,' % str(TYPE))
+    # End of File
+    file.write('$\n')
