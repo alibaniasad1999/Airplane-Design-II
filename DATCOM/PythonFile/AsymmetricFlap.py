27f633a3dd043edacb26fae34fe04e41aef71201
Ali <alibaniasad1999@yahoo.com>
2021-03-28 15:09:25 +0430

    clean asymetric flap file

--- DATCOM/PythonFile/AsymmetricFlap.py
+++ DATCOM/PythonFile/AsymmetricFlap.py
@@ -1,20 +1,13 @@
 import LoopWriter
 # add Asymmetric Flap files to DATCOM #
-# FTYPE = 1.0 plian flaps
-# FTYPE = 2.0  single sloted flaps
-# FTYPE = 3.0 fowler flaps
-# FTYPE = 4.0 double sloted flaps
-# FTYPE = 5.0 split flaps
-# FTYPE = 6.0 leading edge flaps
-# FTYPE = 7.0 lesding edge slats
-# FTYPE = 8.0  kruger
-# $ASYFLP
-# $ASYFLP
-# STYPE = 4.0, SPANFI = 9.523, SPANFO = 12.57,
-# NDELTA = 9.0,
-# DELTAL(1) = -24.0, -20.0, -10.0, -5.0, 0.0, 5.0, 10.0, 20.0, 24.0,
-# DELTAR(1) = 24.0, 20.0, 10.0, 5.0, 0.0, -5.0, -10.0, -20.0, -24.0,
-# CHRDFI = 0.4486, CHRDFO = 0.30122$
+# STYPE = 1.0 plian flaps
+# STYPE = 2.0  single sloted flaps
+# STYPE = 3.0 fowler flaps
+# STYPE = 4.0 double sloted flaps
+# STYPE = 5.0 split flaps
+# STYPE = 6.0 leading edge flaps
+# STYPE = 7.0 lesding edge slats
+# STYPE = 8.0  kruger
 STYPE = 2.0
 # Inboard Span b_i
 SPANFI = 9.523
