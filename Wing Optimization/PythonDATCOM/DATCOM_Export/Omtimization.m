0d8f76b3438a5b404f08a9ff4188044fb0941cb2
Ali <alibaniasad1999@yahoo.com>
2021-05-22 14:05:54 +0430

    add changes for taper

--- Wing Optimization/PythonDATCOM/DATCOM_Export/Omtimization.m
+++ Wing Optimization/PythonDATCOM/DATCOM_Export/Omtimization.m
@@ -1,3 +1,4 @@
+taper = 0.28;
 AR_list = 8.0:.2:11.0;
 Sweep_list = 20.0:1.0:35;
 formatSpecAR = '%.1f'; % show 2 decimal number 
@@ -7,8 +8,8 @@ drag_co_list = zeros(1, 1);
 for i = 1:length(AR_list)
     for j = 1:length(Sweep_list)
         % make file name %
-        name = append('Phoenix50Seat-taper0.30', 'AR',...
-        num2str(AR_list(i), formatSpecAR), '-SW',...
+        name = append('Phoenix50Seat-taper', num2str(taper, '%.2f'),...
+        'AR', num2str(AR_list(i), formatSpecAR), '-SW',...
         num2str(Sweep_list(j), formatSpecSW));
         [cl, cd] = RunDATCOM(name); % run DATCOM
         lift_co_list(i, j) = cl;
--- Wing Optimization/PythonDATCOM/main.py
+++ Wing Optimization/PythonDATCOM/main.py
@@ -1,11 +1,12 @@
 import numpy as np
+taper = 0.28
 AR_list = np.arange(8, 11.2, 0.2).tolist()
 Sweep_list = np.arange(20, 36, 1.0).tolist()
 for i in range(len(AR_list)):
     AR = round(AR_list[i], 1)
     for j in range(len(Sweep_list)):
         Sweep = round(Sweep_list[j], 1)
-        filename = 'DATCOM_Export/Phoenix50Seat-taper0.30' + 'AR' + str(AR) + '-SW' + str(Sweep) + '.dcm'
+        filename = 'DATCOM_Export/Phoenix50Seat-taper' + str(taper) + 'AR' + str(AR) + '-SW' + str(Sweep) + '.dcm'
         file = open(filename, 'w')
         import ControlCards
         ControlCards.control_cards_writer(file)
@@ -21,7 +22,7 @@ for i in range(len(AR_list)):
         Airfoil.airfoil_writer(file)
         # def wing_writer(file, taper, Sref, AR, Sweep):
         import Wing
-        Wing.wing_writer(file, 0.3, 702.4, AR, Sweep)
+        Wing.wing_writer(file, taper, 702.4, AR, Sweep)
         import VerticalTail
         VerticalTail.vertical_tail_writer(file)
         import HorizontalTail
