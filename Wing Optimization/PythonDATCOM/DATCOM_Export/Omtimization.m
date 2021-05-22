efae7f46d31451cc4cc16b304858c7500bfb373b
Ali <alibaniasad1999@yahoo.com>
2021-05-22 13:36:31 +0430

    add comment and delet pause command that was unnessery in matlab file

--- Wing Optimization/PythonDATCOM/DATCOM_Export/Omtimization.m
+++ Wing Optimization/PythonDATCOM/DATCOM_Export/Omtimization.m
@@ -1,18 +1,21 @@
 AR_list = 8.0:.2:11.0;
 Sweep_list = 20.0:1.0:35;
-formatSpecAR = '%.1f';
-formatSpecSW = '%.1f';
+formatSpecAR = '%.1f'; % show 2 decimal number 
+formatSpecSW = '%.1f'; % show 2 decimal number 
 lift_co_list = zeros(1, 1);
 drag_co_list = zeros(1, 1);
 for i = 1:length(AR_list)
     for j = 1:length(Sweep_list)
+        % make file name %
         name = append('Phoenix50Seat-taper0.30', 'AR',...
         num2str(AR_list(i), formatSpecAR), '-SW',...
         num2str(Sweep_list(j), formatSpecSW));
-        [cl, cd] = RunDATCOM(name);
+        [cl, cd] = RunDATCOM(name); % run DATCOM
         lift_co_list(i, j) = cl;
         drag_co_list(i, j) = cd;
+        % show satate %
         fprintf('AR = %f, Sweep = %f\n', AR_list(i), Sweep_list(j));
     end
 end
+delete *.dcm % delete all DATCOM file
         
--- Wing Optimization/PythonDATCOM/DATCOM_Export/RunDATCOM.m
+++ Wing Optimization/PythonDATCOM/DATCOM_Export/RunDATCOM.m
@@ -1,8 +1,9 @@
 function [lift_co, drag_co] = RunDATCOM(name)
 run_name = append(name, '.dcm &');
+% run DATCOM program %
 system(run_name);
-pause(0.2)
 export_name = append(name, '.csv');
+% wait until csv file is ready %
 while ~isfile(export_name)
     pause(0.1)
 end
@@ -10,6 +11,7 @@ data = readtable(export_name);
 %%% End Tasks %%%
 !taskkill -f -im ac3dview.exe
 !taskkill -f -im cmd.exe
+% return data % 
 lift_temp = data.Cd(3);
 drag_temp = data.Cd(1);
 lift_co = str2num(lift_temp{1});
